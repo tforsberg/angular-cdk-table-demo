8f03e11e21296a077ad51fc42f1d6786cdd12554
Maina Wycliffe <wmmaina7@gmail.com>
2018-09-10 16:12:22 +0300

    used CdkTable to display data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { StudentResults } from './student-results';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-cdk-table-demo';
+  public DisplayedColumns: string[] = ['name', 'class', 'marks', 'grade'];
+
+  public StudentDataSource: BehaviorSubject<StudentResults[]>;
+
+  public StudentData: StudentResults[] = [
+    { name: 'John', class: 'Grade 6', marks: '300', grade: 'B' },
+    { name: 'Charles', class: 'Grade 6', marks: '300', grade: 'B' },
+    { name: 'Eunice', class: 'Grade 6', marks: '420', grade: 'A' },
+    { name: 'Kate', class: 'Grade 6', marks: '410', grade: 'A-' },
+    { name: 'Joe', class: 'Grade 6', marks: '310', grade: 'B-' },
+    { name: 'Peter', class: 'Grade 6', marks: '200', grade: 'C' },
+    { name: 'Henry', class: 'Grade 6', marks: '250', grade: 'C+' },
+    { name: 'Paul', class: 'Grade 6', marks: '450', grade: 'A' },
+    { name: 'Pius', class: 'Grade 6', marks: '400', grade: 'A-' },
+    { name: 'Kenendy', class: 'Grade 6', marks: '150', grade: 'D' }
+  ];
+
+  constructor() {
+    this.StudentDataSource = new BehaviorSubject<StudentResults[]>(
+      this.StudentData
+    );
+  }
 }
